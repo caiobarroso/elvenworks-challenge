255161c487019508badd328935aebe2632e4551a
Caio Barroso <caiobarroso511@yahoo.com.br>
2022-01-09 23:10:23 -0300

    guardando senha do db em um env file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const dotenv = require('dotenv');
 const cors = require("cors");
 
+dotenv.config({ path: './.env' });
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
